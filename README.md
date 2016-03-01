61e127160950d8df9b975a37dc6f4c8709b1ff71
Arturo Mejia <arturo.mejia@kreatetechnology.com>
2016-03-01 16:30:09 -0500

    WIP First draft of CLI tool. Updated README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # datadiff
+A library and CLI tool to find differences between a master data source and a slave data source. Two data sources are equal if they contain the same numeric IDs. 
+
+### Strategy
+ - Create a histogram of the numeric IDs on the master data source.
+ - Create a histogram of the numeric IDs on the slave data source.
+ - Merge and compare the histograms.
+ - If the bin capacities are full, mark this range as resolved.
+ - Fetch the histogram of the unresolved bins with smaller bin sizes.
+ - Merge and compare the histograms.
+ - Fetch the ids of the unresolved bins.
+ - Diff the numeric IDs of unresolved bins and output the results.
+
+### Supported Data Sources
+  - mysql
+  - elasticsearch ~0.90.13, ~1.0
+
+### Sample Command Line Usage
+```bash
+ datadiff -interval 200 \
+ -mdriver 'mysql' \
+ -mconn 'root:root@(localhost:3306)/my_db_name?charset=utf8' \
+ -mconf '{"table_name":"my_table_name", "field_name":"my_id_field_name"}' \
+ -sdriver 'elasticsearch' \
+ -sconn 'http://localhost:9200' \
+ -sconf '{"index":"my_index_name", "type":"my_type_name", "field":"my_id_field_path"}'
+ ```
---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/arturom/datadiff/datasource"
+	"gopkg.in/olivere/elastic.v1"
+)
+
+func main() {
+	// Parse flags
+	o := cliOpts{}
+	o.parseFlags()
+
+	// Initialize data sources
+	m, s, err := o.initSources()
+	if err != nil {
+		panic(err)
+	}
+
+	//Do magic here
+	fmt.Println()
+	printThings(m, *o.initialInterval)
+
+	fmt.Println()
+	printThings(s, *o.initialInterval)
+}
+
+func printThings(s datasource.DataSource, interval int) {
+	h, err := s.FetchHistogramAll(interval)
+	if err != nil {
+		panic(err)
+	}
+	for _, b := range h.Bins {
+		fmt.Printf("%#v\n", b)
+	}
+}
+
+type sourceOpts struct {
+	driver     *string
+	config     *string
+	configfile *string
+}
+
+type cliOpts struct {
+	initialInterval *int
+
+	// Options for master source
+	masterDriver     *string
+	masterConnection *string
+	masterConfig     *string
+
+	// Options for slave source
+	slaveDriver     *string
+	slaveConnection *string
+	slaveConfig     *string
+}
+
+func (o *cliOpts) parseFlags() {
+
+	// Parse params for the Master data source
+	o.masterDriver = flag.String("mdriver", "", "Master driver [elasticsearch|mysql]")
+	o.masterConnection = flag.String("mconn", "", "Master connection string")
+	o.masterConfig = flag.String("mconf", "{}", "Master configuration string")
+
+	// Parse params for the Slave data source
+	o.slaveDriver = flag.String("sdriver", "", "Slave driver [elasticsearch|mysql]")
+	o.slaveConnection = flag.String("sconn", "", "Slave connection string")
+	o.slaveConfig = flag.String("sconf", "{}", "Slave configuration string")
+
+	// Parse universal params
+	o.initialInterval = flag.Int("interval", 1000, "Initial histogram interval")
+
+	flag.Parse()
+}
+
+func (o cliOpts) initSources() (m, s datasource.DataSource, err error) {
+	m, err = datasourceFactory(*o.masterDriver, *o.masterConnection, *o.masterConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+	s, err = datasourceFactory(*o.slaveDriver, *o.slaveConnection, *o.slaveConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+	return
+}
+
+func datasourceFactory(driver, connection, config string) (datasource.DataSource, error) {
+	if driver == "mysql" {
+		return initMysql(connection, config)
+	}
+
+	if driver == "elasticsearch" {
+		return initElasticsearch(connection, config)
+	}
+
+	return nil, fmt.Errorf("No datasource matching type: %s", driver)
+}
+
+type mySQLSpecificConfig struct {
+	TableName string `json:"table_name"`
+	FieldName string `json:"field_name"`
+}
+
+func initMysql(connection, config string) (datasource.DataSource, error) {
+	// Unmarshal config String
+	c := mySQLSpecificConfig{}
+	err := json.Unmarshal([]byte(config), &c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Instantiate MySQL connection pool
+	db, err := sql.Open("mysql", connection)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the MySQL server
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	// Return instance of datasource.DataSource
+	return datasource.MysqlDataSource{
+		DB:        db,
+		Tablename: c.TableName,
+		FieldName: c.FieldName,
+	}, nil
+}
+
+type esSpecificConfig struct {
+	IndexName string `json:"index"`
+	TypeName  string `json:"type"`
+	FieldName string `json:"field"`
+}
+
+func initElasticsearch(connection, config string) (datasource.DataSource, error) {
+	// Unmarshal config String
+	c := esSpecificConfig{}
+	err := json.Unmarshal([]byte(config), &c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Instantiate an Elasticsearch client
+	client, err := elastic.NewClient(http.DefaultClient, connection)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping an Elasticsearch node
+	_, _, err = client.Ping().URL(connection).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	// Return instance of datasource.DataSource
+	return datasource.ES0DataSource{
+		Client:    *client,
+		IndexName: c.IndexName,
+		TypeName:  c.TypeName,
+		FieldName: c.FieldName,
+	}, nil
+}
